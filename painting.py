352d9503d8856df16de7055f97908d8fbf9824e9
alex-kuznetsov7677 <a.kuznetsov7677@gmail.com>
2025-02-12 00:47:06 +0300

    Saved changes

--- painting.py
+++ painting.py
@@ -4,20 +4,18 @@ import matplotlib.pyplot as plt
 def plot_trade_results(csv_file):
     df = pd.read_csv(csv_file)
 
-    fig, axs = plt.subplots(4, 1, figsize=(12, 8), sharex=True)
+    fig, axs = plt.subplots(3, 1, figsize=(12, 6), sharex=True)
 
     axs[0].plot(df['time_step'], df['best_bid'], label='Best Bid', color='blue')
     axs[0].plot(df['time_step'], df['best_ask'], label='Best Ask', color='red')
     axs[0].set_title('Best Bid & Ask')
     axs[0].legend()
 
-    axs[1].plot(df['time_step'], df['assets'], label='Assets Bull', color='green')
-
+    axs[1].plot(df['time_step'], df['assets'], label='Assets', color='green')
     axs[1].set_title('Assets Over Time')
     axs[1].legend()
 
-    axs[2].plot(df['time_step'], df['total_profit'], label='Cash Bull', color='green')
-
+    axs[2].plot(df['time_step'], df['total_profit'], label='Total Profit', color='red')
     axs[2].set_title('Total Profit Over Time')
     axs[2].legend()
 
